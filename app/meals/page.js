4c747469cc47d92082c71908b8bc7f8843350f92
Suhas Bachhav <ksb.suhas@gmail.com>
2025-03-30 13:16:26 +0530

    meals UI setup

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,6 +1,23 @@
+import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+
 function MealsPage() {
     return ( 
-        <h2>Meal Page</h2>
+        <>
+            <header className={classes.header}>
+                <h1>Delicious Meals, created <span className={classes.highlight}>by you</span></h1>
+                <p>
+                    Choose you favorite recipe and cook it by yourself! It is easy and fun! 
+                </p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">Share your favorite recipe</Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                <MealsGrid meals={[]}/>
+            </main>
+        </>
      );
 }
 
--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -3,6 +3,7 @@ import logoImg from '@/assets/logo.jpg';
 import classes from '@/components/main-header/main-header.module.css';
 import Image from "next/image";
 import MainHeaderBackgroundPage from "./main-header-background";
+import NavLink from "./nav-link";
 
 function MainHeaderPage() {
     return ( 
@@ -17,10 +18,14 @@ function MainHeaderPage() {
                 <nav className={classes.nav}>
                     <ul>
                         <li>
-                            <Link href='/meals'>Browse Meals</Link>
+                            <NavLink href='/meals'>
+                                Browse Meals
+                            </NavLink>
                         </li>
                         <li>
-                            <Link href='/community'>Foodies Community</Link>
+                            <NavLink href='/community'>
+                                Foodies Community
+                            </NavLink>
                         </li>
                     </ul>
                 </nav>
