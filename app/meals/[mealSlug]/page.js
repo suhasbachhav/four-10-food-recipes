c149057889a92d591fdf558431049c76fd0d5acf
Suhas Bachhav <ksb.suhas@gmail.com>
2025-03-30 14:21:21 +0530

    meal details

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,11 +1,34 @@
-function MealDetailsPage() {
+import Image from 'next/image';
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
+
+async function MealDetailsPage({params}) {
+    const meal = await getMeal(params.mealSlug);
+
+    meal.instructions = meal.instructions.replace(/\n/g, '<br>');
     return ( 
         <>
-            <header>
-
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image}
+                    alt={meal.title}
+                    fill/>
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                         By <a href={`mailto:${meal.creator_email}`}></a>
+                    </p>
+                    <p className={classes.summary}>
+                        {meal.summary}
+                    </p>
+                </div>
             </header>
             <main>
-                
+                <p className={classes.instructions}
+                    dangerouslySetInnerHTML={{__html:meal.instructions}}
+                >
+                </p>
             </main>
         </>
      );
