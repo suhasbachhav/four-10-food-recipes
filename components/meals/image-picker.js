2ef01710681634d67777b89a84718340fc91de99
Suhas Bachhav <ksb.suhas@gmail.com>
2025-03-30 16:00:18 +0530

    image picker

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,35 +1,36 @@
 'use client';
 
 import { useRef } from 'react';
-import classes from './image-picker.module.css'
 
-function ImagePicker({label, name}) {
-    const imageInput = useRef();
-    function handlePickerClick(){
-        imageInput.current.click()
-    }
+import classes from './image-picker.module.css';
 
-    return ( 
-        <div className={classes.picker}>
-            <label htmlFor={name}>
-                {label}
-            </label>
-            <div className={classes.controls}>
-                <input 
-                className={classes.input}
-                type='file' 
-                id={name}
-                accept='image/jpeg, image/png, image/jpg' 
-                name={name}
-                ref={imageInput}
-                />
-                <button onClick={handlePickerClick} className={classes.button} type='button'>
-                    Pick an Image
-                </button>
-            </div>
+export default function ImagePicker({ label, name }) {
+  const imageInput = useRef();
 
-        </div>
-     );
+  function handlePickClick() {
+    imageInput.current.click();
+  }
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={imageInput}
+        />
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handlePickClick}
+        >
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
 }
-
-export default ImagePicker;
